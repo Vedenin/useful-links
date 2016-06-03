f112e362f3fb202c1d85e27587353b716d756070
vvedenin <vvedenin@luxoft.com>
2016-06-03 20:13:58 +0200

    Add languages and popular languages to Constants

---
+++ sources/src/test/java/com/github/vedenin/useful_links/ConstantTests.java
@@ -0,0 +1,23 @@
+package com.github.vedenin.useful_links;
+
+import org.junit.Test;
+
+import static com.github.vedenin.useful_links.Constants.LANGUAGES;
+import static com.github.vedenin.useful_links.Constants.POPULAR_LANGUAGES;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by vvedenin on 6/3/2016.
+ */
+public class ConstantTests {
+    @Test
+    public void testLanguages() {
+        assertEquals(408, LANGUAGES.size());
+    }
+
+    @Test
+    public void testPopularLanguages() {
+        assertEquals(44, POPULAR_LANGUAGES.size());
+    }
+
+}
